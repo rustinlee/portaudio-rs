9492cdd6fa830ef580a98fae29bd07c5a70a0f2a
Rustin Wollin <rwollin96@gmail.com>
2017-03-29 10:31:43 -0400

    Attempt to fix MSVC cross compilation target

--- portaudio-sys/build.rs
+++ portaudio-sys/build.rs
@@ -1,15 +1,5 @@
 extern crate pkg_config;
 
-#[cfg(not(windows))]
-fn main() {
-    match pkg_config::find_library("portaudio-2.0")
-    {
-        Ok(..) => {},
-        Err(e) => panic!("{}", e),
-    }
-}
-
-#[cfg(windows)]
 fn main() {
     // Assume the library is in the correct path
 }
